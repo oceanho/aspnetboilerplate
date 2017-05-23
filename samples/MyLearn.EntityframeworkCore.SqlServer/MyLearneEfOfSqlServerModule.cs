4e1083bc8dd57094b6aad9f778ba738ad28017cc
hehai <gzhehai@foxmail.com>
2017-05-23 20:43:02 +0800

    Updated fixed some path

---
+++ samples/MyLearn.EntityframeworkCore.SqlServer/MyLearneEfOfSqlServerModule.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Modules;
+using Abp.Reflection.Extensions;
+using MyLearn.Common;
+using System;
+
+namespace MyLearn.EntityframeworkCore.SqlServer
+{
+    [DependsOn(typeof(MyLearnCommonModule))]
+    public class MyLearneEfOfSqlServerModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(GetType().GetAssembly());
+        }
+    }
+}
