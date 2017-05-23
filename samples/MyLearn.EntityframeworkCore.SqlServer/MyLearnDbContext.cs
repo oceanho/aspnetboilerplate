90c3fc779f2d6f897336a87057fbd0ac51ea7658
hehai <gzhehai@foxmail.com>
2017-05-23 20:37:47 +0800

    Added ORM support of sqlserver

---
+++ samples/MyLearn.EntityframeworkCore.SqlServer/MyLearnDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Abp.Dependency;
+
+namespace MyLearn.EntityframeworkCore
+{
+    public class MyLearnDbContext : MyLearnCommonDbContext
+    {        
+        public MyLearnDbContext(DbContextOptions options) : base(options)
+        {            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            // SqlServer
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
