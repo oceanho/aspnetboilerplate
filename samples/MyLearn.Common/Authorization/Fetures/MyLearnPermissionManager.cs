62763d5c2cbae530f8677b7fe4b238f633537a26
hehai <gzhehai@foxmail.com>
2017-05-27 17:30:59 +0800

    Added some fetures note

---
+++ samples/MyLearn.Common/Authorization/Fetures/MyLearnPermissionManager.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.MultiTenancy;
+
+namespace MyLearn.Common.Authorization.Fetures
+{
+    public class MyLearnPermissionManager : IPermissionManager
+    {
+        public IReadOnlyList<Permission> GetAllPermissions(bool tenancyFilter = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<Permission> GetAllPermissions(MultiTenancySides multiTenancySides)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Permission GetPermission(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Permission GetPermissionOrNull(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ samples/MyLearn.Web.Api/MyLearnWebApiModule.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Modules;
+using MyLearn.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MyLearn.Web.Api
+{
+    [DependsOn(typeof(MyLearnKernelModule))]
+    public class MyLearnWebApiModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+        }
+        public override void Initialize()
+        {
+            Configuration.ap
+        }
+
+        public override void PostInitialize()
+        {
+        }
+    }
+}
